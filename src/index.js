0bbeca357ffd5c9977cd882761b46dda62e5e09f
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2023-06-07 08:36:51 +0100

    Commented some code, implemented pages up to page 17.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import puppeteer, { Browser, Page } from "puppeteer-core";
 import { executablePath } from 'puppeteer';
-import { sleep, randomInt, toId, toClass } from './utils.js'
-
-const nextButton = "NextButton";
-
-const firstSID = "QR~QID4";
-
-const sndDate = "QR~QID118~2";
-const sndDateHighlighted = "ui-state-highlight";
-
-const sndHour = "QR~QID8#1~1";
-
-const sndMin = "QR~QID8#2~1";
-
-const sndAmPm = "QR~QID8#3~1";
-
+import { sleep, randomInt, toId, toClass, randomUniqueInts, oneInX, chooseOneAtRandom, chooseXAtRandom } from './utils.js'
 
 /**
 * BurgerQueen
-* @constructor
+* @constructor Unused, run() method handles initialization by calling the initalize() method.
 */
 class BurgerQueen {
 
+    /**
+     * Handles the initialization of the bot, including the creation of the browser and the page and the navigation to the desired page.
+     */
     async initialize() {
+        const pageURL = "https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=PRT&Q_Language=PT&PT=1";
         try {
             this.browser = await puppeteer.launch({
                 args: ['--no-sandbox',],
@@ -33,7 +23,7 @@ class BurgerQueen {
             });
 
             this.page = await this.browser.newPage();
-            await this.page.goto("https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=PRT&Q_Language=PT&PT=1");
+            await this.page.goto(pageURL);
             await this.page.waitForSelector(toId(firstSID));
 
         }
@@ -43,11 +33,22 @@ class BurgerQueen {
         }
     }
 
+    /**
+     * Clicks on the element with the specified selector.
+     * 
+     * @param {String} selector name of the selector to click, should be an ID.
+     */
     async click(selector) {
         await this.page.waitForSelector(selector);
         await this.page.click(selector);
     }
 
+    /**
+     * Types the given text in the DOM element with the specified selector.
+     * 
+     * @param {String} selector text field selector, should be an ID. 
+     * @param {String} text text to write in the text field.
+     */
     async type(selector, text) {
         await this.page.waitForSelector(selector);
         await this.page.type(selector, text);
@@ -58,14 +59,34 @@ class BurgerQueen {
         await this.page.select(selector, value);
     }
 
-    async firstPage() {
-        // todo pedir um ao utilizador
-        this.type(toId(firstSID), "21595");
+    async goToNextPage() {
+        const nextButton = "NextButton";
         await sleep(2000);
         this.click(toId(nextButton));
     }
 
-    async secondPage() {
+    async dealWithRadioButtons(selectorStart, satisfiedMin, satisfiedMax, tableRowMin, tableRowMax) {
+        for (let i = tableRowMin; i < tableRowMax; i++) {
+            const howSatisfied = randomInt(satisfiedMin, satisfiedMax);
+            const buttonLabel = `${selectorStart}~${i}~${howSatisfied}`
+            this.click(toId(buttonLabel));
+        }
+    }
+
+    firstPage() {
+        const firstSID = "QR~QID4";
+        // todo pedir um ao utilizador
+        this.type(toId(firstSID), "21595");
+        this.goToNextPage();
+    }
+
+    secondPage() {
+        const sndDate = "QR~QID118~2";
+        const sndDateHighlighted = "ui-state-highlight";
+        const sndHour = "QR~QID8#1~1";
+        const sndMin = "QR~QID8#2~1";
+        const sndAmPm = "QR~QID8#3~1";
+
         this.click(toId(sndDate));
         this.click(toClass(sndDateHighlighted));
         
@@ -80,6 +101,160 @@ class BurgerQueen {
         this.select(toId(sndMin), `${date.getMinutes()}`);
         this.select(toId(sndAmPm), `${ampm}`);
 
+        this.goToNextPage();
+    }
+
+    async thirdPage() {
+        const selfServiceButton = "QID12-1-label";
+        this.click(toId(selfServiceButton));
+        goToNextPage();
+    }
+
+    async fourthPage() {
+        const dineInButton = "QID14-2-label";
+        this.click(toId(dineInButton));
+        goToNextPage();
+    }
+
+    async fifthPage() {
+        const howSatisfied = randomInt(17, 18);
+        const buttonLabel = `QID18-${howSatisfied}-label`
+        this.click(toId(buttonLabel));
+        goToNextPage();
+    }
+
+    async sixthPage() {
+        const phrases = [
+            "A comida estava ótima!",
+            "O serviço foi bom!",
+            "O atendimento foi bom!",
+            "O atendimento foi rápido!",
+            "O atendimento foi eficiente!",
+            "O atendimento foi simpático!",
+            "O atendimento foi prestável!",
+            "O atendimento foi atencioso!",
+            "O atendimento foi profissional!",
+            "O estabelecimento estava limpo!",
+            "O estabelecimento estava organizado!",
+        ];
+
+        const textAreaLabel = "QR~QID117";
+
+        const selectedPhrases = chooseXAtRandom(phrases, 3);
+
+        for (const phrase in selectedPhrases) 
+            this.type(toId(textAreaLabel), phrase);
+
+        this.goToNextPage();
+    }
+
+    async seventhPage() {
+        this.dealWithRadioButtons("QR~QID121", 12, 14, 4, 10);
+        this.goToNextPage();
+    }
+
+    async eighthPage() {
+        
+        const howSatisfied1 = randomInt(12, 14);
+        const buttonLabel1 = `QR~QID123~7~${howSatisfied1}`;
+        this.click(toId(buttonLabel1));
+
+        const howSatisfied2 = randomInt(12, 14);
+        const buttonLabel2 = `QR~QID123~9~${howSatisfied2}`;
+        this.click(toId(buttonLabel2));
+    
+        this.goToNextPage();
+    }
+
+    async ninthPage() {
+        const howSatisfied = randomInt(29, 30);
+        if (oneInX(4)) {
+            howSatisfied = 8;
+        }
+        if (oneInX(4)) {
+            howSatisfied = 33;
+        }
+
+        const buttonLabel = `QID122-${howSatisfied}-label`;
+        
+        this.click(toId(buttonLabel));
+
+        this.goToNextPage();
+    }
+
+    async tenthPage() {
+        this.dealWithRadioButtons("QR~QID21", 12, 14, 1, 5);
+        
+        this.goToNextPage();
+    }
+
+
+    async eleventhPage() {
+        this.dealWithRadioButtons("QR~QID22", 8, 10, 1, 5);
+
+        this.goToNextPage();
+    }
+
+    async twelfthPage() {
+        const buttonLabel = "QID38-2-label";
+        this.click(toId(buttonLabel));
+        
+        this.goToNextPage();
+    }
+
+    async thirteenthPage() {
+        this.dealWithRadioButtons("QR~QID41", 6, 7, 1, 3);
+
+        this.goToNextPage();
+    }
+
+    async fourteenthPage() {
+        const possibleChoices = [
+            "QID46-169-label",
+            "QID46-294-label",
+            "QID46-266-label",
+            "QID46-272-label",
+            "QID46-311-label",
+        ];
+
+        const choice = chooseOneAtRandom(possibleChoices);
+
+        this.click(toId(choice));
+
+        this.goToNextPage();
+    }
+
+    async fifteenthPage() {
+        const choice = undefined;
+        if (oneInX(3)) {
+            if (oneInX(2)) {
+                choice = "QID47-114-label";
+            } 
+            else {
+                choice = "QID47-118-label"
+            }
+        }
+        else {
+            choice = "QID47-128-label";
+        }
+
+        this.click(toId(choice));
+        
+        this.goToNextPage();
+    }
+
+    async sixteenthPage() {
+        const choice = chooseOneAtRandom(["QID48-12-label", "QID48-13-label", "QID48-14-label"]);
+
+        this.click(toId(choice));
+
+        this.goToNextPage();
+    }
+
+    async seventeenthPage() {
+        this.dealWithRadioButtons("QR~QID49", 12, 14, 1, 10);
+
+        this.goToNextPage();
     }
 
     async run() {
--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,20 @@
 import "./css.escape.js";
 
 /**
- * Await this function to sleep for a given number of milliseconds
+ * Await this function to sleep for a given number of milliseconds.
  * 
- * @param {*} ms number of milliseconds to sleep for
+ * @param {number} ms number of milliseconds to sleep for
  * @returns Promise that resolves after ms milliseconds
  */
 export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-/** Returns a random integer between min and max, inclusive
+/** 
+ * Returns a random integer between min and max, inclusive.
  * 
- * @param {*} min the minimum value 
- * @param {*} max the maximum value
+ * @param {number} min minimum value 
+ * @param {*} max maximum value
  * @returns the random integer
  */
 export function randomInt(min, max) {
@@ -22,8 +23,8 @@ export function randomInt(min, max) {
 
 /**
  * 
- * @param {*} name selector name to be escaped
- * @returns a selector of an id with the given name
+ * @param {String} name selector name to be escaped
+ * @returns {String} a selector of an id with the given name
  */
 export function toId(name) {
     return "#" + CSS.escape(name);
@@ -31,9 +32,67 @@ export function toId(name) {
 
 /**
  * 
- * @param {*} name selector name to be escaped
- * @returns a selector of a class with the given name
+ * @param {String} name selector name to be escaped
+ * @returns {String} a selector of a class with the given name
  */
 export function toClass(name) {
     return "." + CSS.escape(name);
+}
+
+
+/** 
+ * Returns an array with x unique numbers from the specified range.
+ * 
+ * @param {number} min minimum value 
+ * @param {number} max maximum value
+ * @param {number} x number of unique numbers to return
+ * @returns {Array<number>} array with x unique numbers from the specified range
+ */
+export function randomUniqueInts(min, max, x) {
+    if (max - min < x) {
+        throw new Error("The range is too small to generate " + x + " unique numbers.");
+    }
+
+    let arr = [];
+    while (arr.length < x) {
+        let r = randomInt(min, max);
+        if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+}
+
+/**
+ * Returns true with a probability of 1/x.
+ * 
+ * @param {number} x
+ * @returns {boolean}
+ */
+export function oneInX(x) {
+    return randomInt(0, x) === 0;
+}
+
+/** Returns one of the elements of the given array at random.
+ * 
+ * @param {Array<T>} arr
+ * @returns T
+ */
+export function chooseOneAtRandom(arr) {
+    if (arr.length === 0) {
+        throw new Error("The array is empty.");
+    }
+
+    return arr[randomInt(0, arr.length)];
+}
+
+export function chooseXAtRandom(arr, x) {
+    if (arr.length === 0) {
+        throw new Error("The array is empty.");
+    }
+    const returnArr = [];
+
+    while (returnArr.length < x) {
+        let r = randomInt(0, arr.length);
+        if (returnArr.indexOf(r) === -1) returnArr.push(arr[r]);
+    }
+    return returnArr;
 }
